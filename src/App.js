07705fba2b75f66772d9b23c381e3befaa4cba40
yeonjae <yeonjae978@gmail.com>
2023-05-11 14:15:16 +0900

    2023-05-11 잔재된 파일들 커밋

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
-// import Counter from './Counter';
-// import MyComponent from './MyComponent';
-// import Say from './Say';
-// import EventPractice from './EventPractice';
-// import EventPracticeFn from './EventPracticeFn';
-// import ValidationSample from './ValidationSample';
-import ScrollBox from './ScrollBox';
-import IterationSample from './IterationSample';
 import React, { Component } from 'react';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
+
+function getRandomColor(){
+  return '#' + Math.floor(Math.random() * Math.pow(2, 24)).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+  handleClick = () => {
+    this.setState({
+      color:getRandomColor()
+    })
+  }
   render() {
     return (
-      // <div>
-      //   <ScrollBox ref={(ref) => this.scrollBox=ref}/>
-      //   <button onClick={() => this.scrollBox.scrollToBottom()}>
-      //     맨 밑으로
-      //   </button>
-      //   <button onClick={() => this.scrollBox.scrollToTop()}>
-      //     맨 위로
-      //   </button>
-      // </div>
       <div>
-        <IterationSample />
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color}/>
+        </ErrorBoundary>
       </div>
     );
   }
 }
-export default App;
-// export default App;
 
-// const App = () => {
-//   return <ValidationSample />;
-// };
+export default App;
 
